eca812a0e01ccaa095ad60f4f13509e7e7762b32
aswajith <aswajith.jithu888@gmail.com>
2022-08-12 17:19:42 +0530

    testing bootstrap model

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,37 @@
 console.log("main.js is loaded");
+
 $(function(){
 
     $("td.user").find(".btn-del").on("click",function(){
-     let id=$(this).attr('id');
-     $.ajax({
-        type: "POST",
-        url: "/me/delete/user",
-        data:{id:id},
-        dataType: 'json',
-        success: function(){
-            console.log('success');
-            window.location.reload();
-        },
-        error: function(){
-           alert('something went wrong');
-        }
+     let id=$(this).attr('oid');
+     sendData("/me/delete/user",{id:id})
+    });
+
+    $("td.user").find(".btn-update").on("click",function(){
+        let id=$(this).attr('oid');
+        sendData("/me/update/user",{id:id})
+       });
+
+       $('#modelTrigger').on('click',function(){
+        alert("btn click")
+        $('#myModal').modal('show')
         });
-    })
+
+    function sendData(url,data)
+    {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data:data,
+            dataType: 'json',
+            success: function(){
+                console.log('success');
+                window.location.reload();
+            },
+            error: function(){
+               alert('something went wrong');
+            }
+            });
+    }
    
    });
--- routes/me.js
+++ routes/me.js
@@ -14,6 +14,7 @@ router.get('/form', function(req, res, next) {
 router.post('/register', userController.register);
 router.post('/update', userController.update);
 router.post('/delete/user', userController.deleteUser);
+//router.get('/update/user', userController.userModel);
 
 
 router.get('/list', userController.list);
